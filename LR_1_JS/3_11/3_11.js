7fe3c5a83010e44b061daf77d995fd60eb5eeed9
Yaroslav-Ludchak1234 <yaroslav.ludchak-pi1901@kep.nung.edu.ua>
2022-09-29 20:24:26 +0300

    kgg
    Signed-off-by: Yaroslav-Ludchak1234 <yaroslav.ludchak-pi1901@kep.nung.edu.ua>

--- LR_1_JS/3_11/3_11.js
+++ LR_1_JS/3_11/3_11.js
@@ -1,19 +1,20 @@
 let res = document.getElementById("result");
 let flag = 1;
-
 function show(n) {
-    res.innerHTML += "<div class='result__item'>" + n + "</div>";
+	res.innerHTML+= "<div class='result__item'>" + n + "</div>";
 }
 
-function check(n) {
-    if (n == null) {
-        flag = 0;
-    } else {
-        show(n);
-    }
+function check(n){
+	if( n == null){
+		flag = 0;
+	}
+	else{
+		show(n);
+	}	
 }
 
-while (flag) {
-    let n = prompt("Прізвище і ім'я студента:");
-    check(n);
+while(flag){
+	let n = prompt("Прізвище і ім'я студента:");
+	check(n);
 }
+

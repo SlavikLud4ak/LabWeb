3656a29dfb0362501b6ec02d27c24d44e8a4cd68
Yaroslav-Ludchak1234 <yaroslav.ludchak-pi1901@kep.nung.edu.ua>
2022-11-11 11:41:42 +0200

    update
    Signed-off-by: Yaroslav-Ludchak1234 <yaroslav.ludchak-pi1901@kep.nung.edu.ua>

--- PR_5_JS/script.js
+++ PR_5_JS/script.js
@@ -1,226 +1,223 @@
-$(document).ready(function() {
-    let listData = JSON.parse(localStorage.getItem('list')) || [];
+const formCreate = document.getElementById('form-create')
+const formEdit = document.getElementById('form-edit')
+const listGroupTodo = document.getElementById('list-group-todo')
+    // const messageCreate = document.getElementById('message-create')
+const time = document.getElementById('time')
+const modal = document.getElementById('modal')
+const overlay = document.getElementById('overlay')
+    /* time elements */
+const fullDay = document.getElementById('full-day')
+const hourEl = document.getElementById('hour')
+const minuteEl = document.getElementById('minute')
+const secondEl = document.getElementById('second')
+const closeEl = document.getElementById('close')
 
+let editItemId
 
-    function update() {
-        localStorage.setItem('list', JSON.stringify(listData));
-        $('.list-group').html('');
-        for (let i = 0; i < listData.length; i++) {
-            createList(listData[i].title, i);
-            if (listData[i].completed) {
-                let checkbox = $('.list-group input[type=checkbox]');
-                $(checkbox[i]).next().addClass('line-through');
-                checkbox[i].checked = true;
-            }
-        }
-        countItems();
-    }
-    update();
+// check
+let todos = JSON.parse(localStorage.getItem('list')) ?
+    JSON.parse(localStorage.getItem('list')) : []
 
+if (todos.length) showTodos()
 
-    function countItems() {
-        let getLen = listData.length;
-        let checkbox = $('.list-group input[type=checkbox]');
-        let completedAry = [];
-        for (let i = 0; i < checkbox.length; i++) {
-            if (checkbox[i].checked) {
-                completedAry.push(i);
-            }
-        }
-        let len = getLen - completedAry.length;
+// setTodos to localstorage
+function setTodos() {
+    localStorage.setItem('list', JSON.stringify(todos))
+}
 
-        $('#countItems').text(len + ' items left')
-    }
+// time
 
-    function active() {
-        for (let i = 0; i < listData.length; i++) {
-            if (listData[i].completed) {
-                let checkbox = $('.list-group input[type=checkbox]');
-                $(checkbox[i]).parent().addClass('d-none');
-            }
-        }
-    }
+function getTime() {
+    const now = new Date()
+    const date = now.getDate() < 10 ? '0' + now.getDate() : now.getDate()
+    const month =
+        now.getMonth() < 10 ? '0' + (now.getMonth() + 1) : now.getMonth()
+    const year = now.getFullYear()
 
-    function completed() {
-        for (let i = 0; i < listData.length; i++) {
-            if (!listData[i].completed) {
-                let checkbox = $('.list-group input[type=checkbox]');
-                $(checkbox[i]).parent().addClass('d-none');
-            }
-        }
-    }
+    const hour = now.getHours() < 10 ? '0' + now.getHours() : now.getHours()
+    const minute =
+        now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes()
+    const second =
+        now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds()
 
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ]
 
-    function nowHash() {
-        if (location.hash === '#active') {
-            update();
-            active();
-        } else if (location.hash === '#completed') {
-            update();
-            completed();
-        } else {
-            update();
-        }
-    }
+    const month_title = now.getMonth()
+    fullDay.textContent = `${date} ${months[month_title]}, ${year} `
 
+    hourEl.textContent = hour
+    minuteEl.textContent = minute
+    secondEl.textContent = second
 
-    function createList(text, num) {
-        const createLi = document.createElement('li');
-        const createInput = document.createElement('input');
-        const createP = document.createElement('p');
-        const createBtn = document.createElement('button');
+    return `${hour}:${minute}, ${date}.${month}.${year}`
+}
 
-        createLi.setAttribute('class', 'list-group-item');
-        createLi.setAttribute('data-num', num);
-        createInput.setAttribute('class', 'mr-3');
-        createInput.setAttribute('type', 'checkbox');
-        createP.setAttribute('class', 'd-inline');
-        createP.textContent = text;
-        createBtn.setAttribute('class', 'close');
-        createBtn.setAttribute('type', 'button');
-        createBtn.innerHTML = '<span aria-hidden="true">&times;</span>';
+setInterval(getTime, 1000)
 
-        createLi.appendChild(createInput);
-        createLi.appendChild(createP);
-        createLi.appendChild(createBtn);
-        document.querySelector('.list-group').appendChild(createLi);
-    }
+// show todos
+function showTodos() {
+    const todos = JSON.parse(localStorage.getItem('list'))
+    listGroupTodo.innerHTML = ''
+    todos.forEach((item, i) => {
+        listGroupTodo.innerHTML += `
+        <li  ondblclick="setCompleted(${i})" class="list-group-item d-flex justify-content-between ${
+      item.completed == true ? 'complated' : ''
+    } ">
+             ${item.text}
+        <div class="todo-icons">
+          <span class="opacity-50 me-2">${item.time}</span>
+          <i class="fas fa-pen me-2 text-warning" onclick=(editTodo(${i})) src="img/edit.svg" alt="edit icon" width="25" height="25"></i>
+          <i class="fas fa-trash text-danger" onclick=(deleteTodo(${i}))  src="img/delete.svg" alt="delete icon" width="25" height="25"></i>
+        </div>
+      </li>
+    `
+    })
+}
 
-
-    $('#addList').keyup(function(e) {
-        if (e.keyCode === 13) {
-            let textValue = $(this).val();
-            if (textValue === '') { return };
-            let items = {};
-            items.title = textValue;
-            items.completed = false;
-            listData.push(items);
-            nowHash();
-            $(this).val('');
-        }
-    });
-
-    $('.nav-pills').click(function(e) {
-        $(this).find('a').removeClass('active');
-        if (e.target.tagName !== 'A') { return }
-        $(e.target).addClass('active')
-        switch (e.target.hash) {
-            case '#active':
-                update();
-                active();
-                break;
-            case '#completed':
-                update();
-                completed();
-                break;
-            default:
-                update();
-                break;
-        }
-    });
-
-
-    $('.list-group').click(function(e) {
-        let tagName = e.target.tagName;
-        switch (tagName) {
-            case 'SPAN':
-
-                let num = $(e.target).parent().parent().data('num');
-                listData.splice(num, 1);
-                nowHash();
-                break;
-            case 'INPUT':
-
-                $(e.target).next().toggleClass('line-through');
-                let checkbox = $('.list-group input[type=checkbox]');
-                for (let i = 0; i < checkbox.length; i++) {
-                    if (checkbox[i].checked) {
-                        listData[i].completed = true;
-                    } else {
-                        listData[i].completed = false;
-                    }
-                }
-                nowHash();
-                break;
-        }
-    });
-
-
-    function edit(who) {
-        let num = $(who).data('num');
-
-        let str = `
-			<input type="text" class="edit-line w-100 p-2" data-num="${num}">
-        `
-        $(who).addClass('p-0');
-        $(who).html(str);
-
-        $(who).find('input').val('').focus().val(listData[num].title);
-
-        $(who).find('input').keyup(function(e) {
-            if (e.keyCode === 13) {
-                let textValue = $(this).val();
-                if (textValue === '') { return };
-                listData[num].title = textValue;
-                nowHash();
-            } else if (e.keyCode === 27) {
-                cancelEdit(who, num);
-            }
-        });
-    }
-
-
-    function cancelEdit(who, num) {
-        $(who).removeClass('p-0');
-        $(who).html('');
-        const createInput = document.createElement('input');
-        const createP = document.createElement('p');
-        const createBtn = document.createElement('button');
-
-        createInput.setAttribute('class', 'mr-3');
-        createInput.setAttribute('type', 'checkbox');
-        createP.setAttribute('class', 'd-inline');
-        createP.textContent = listData[num].title;
-        createBtn.setAttribute('class', 'close');
-        createBtn.setAttribute('type', 'button');
-        createBtn.innerHTML = '<span aria-hidden="true">&times;</span>';
-
-        $(who).append(createInput);
-        $(who).append(createP);
-        $(who).append(createBtn);
-
-        if (listData[num].completed) {
-            let checkbox = $('.list-group input[type=checkbox]');
-            $(checkbox[num]).next().addClass('line-through');
-            checkbox[num].checked = true;
-        }
-    }
-
-
-    $('.list-group').dblclick(function(e) {
-        let tagName = e.target.tagName;
-        if (tagName !== 'LI' && tagName !== 'P') {
-            return
-        } else if ($(this).find('input[type=text]').hasClass('edit-line')) {
-            let who = $(this).find('input[type=text]').parent();
-            let num = $(this).find('input[type=text]').data('num');
-            cancelEdit(who, num);
-        }
-        switch (tagName) {
-            case 'LI':
-                edit(e.target);
-                break;
-            case 'P':
-                let getLi = $(e.target).parent();
-                edit(getLi);
-                break;
+function showTodosComplete() {
+    const todos = JSON.parse(localStorage.getItem('list'))
+    listGroupTodo.innerHTML = ''
+    todos.forEach((item, i) => {
+        if (item.completed == true) {
+            listGroupTodo.innerHTML += `
+        <li  ondblclick="setCompleted(${i})" class="list-group-item d-flex justify-content-between ${ item.completed == true ? 'complated' : '' } ">
+             ${item.text}
+        <div class="todo-icons">
+          <span class="opacity-50 me-2">${item.time}</span>
+          <i class="fas fa-pen me-2 text-warning" onclick=(editTodo(${i})) src="img/edit.svg" alt="edit icon" width="25" height="25"></i>
+          <i class="fas fa-trash text-danger" onclick=(deleteTodo(${i}))  src="img/delete.svg" alt="delete icon" width="25" height="25"></i>
+        </div>
+      </li>
+    `
         }
     })
+}
 
+function showTodosNonComplete() {
+    const todos = JSON.parse(localStorage.getItem('list'))
+    listGroupTodo.innerHTML = ''
+    todos.forEach((item, i) => {
+        if (item.completed == false) {
+            listGroupTodo.innerHTML += `
+        <li  ondblclick="setCompleted(${i})" class="list-group-item d-flex justify-content-between ${ item.completed == true ? 'complated' : '' } ">
+             ${item.text}
+        <div class="todo-icons">
+          <span class="opacity-50 me-2">${item.time}</span>
+          <i class="fas fa-pen me-2 text-warning" onclick=(editTodo(${i})) src="img/edit.svg" alt="edit icon" width="25" height="25"></i>
+          <i class="fas fa-trash text-danger" onclick=(deleteTodo(${i}))  src="img/delete.svg" alt="delete icon" width="25" height="25"></i>
+        </div>
+      </li>
+    `
+        }
+    })
+}
 
-    $('#clearAll').click(function(e) {
+// show error
+function showMessage(where, message) {
+    document.getElementById(`${where}`).textContent = message
 
-        listData = [];
-        update();
-    });
-});
+    setTimeout(() => {
+        document.getElementById(`${where}`).textContent = ''
+    }, 2500)
+}
+
+/*<
+ */
+// get Todos
+formCreate.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const todoText = formCreate['input-create'].value.trim()
+    formCreate.reset()
+    if (todoText.length) {
+        todos.push({ text: todoText, time: getTime(), completed: false })
+        setTodos()
+        showTodos()
+    } else {
+        showMessage('message-create', 'Please, Enter some todo...')
+    }
+})
+
+// delete todo
+function deleteTodo(id) {
+    if (confirm("Чи справді ви бажаєте видалити цей запис?")) {
+        const deletedTodos = todos.filter((item, i) => {
+            return i !== id
+        })
+
+        todos = deletedTodos
+        setTodos()
+        showTodos()
+    }
+}
+
+// setCompleted
+function setCompleted(id) {
+    const completedTodos = todos.map((item, i) => {
+        if (id == i) {
+            return {...item, completed: item.completed == true ? false : true }
+        } else {
+            return {...item }
+        }
+    })
+    todos = completedTodos
+    setTodos()
+    showTodos()
+}
+
+// edit Form
+formEdit.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const todoText = formEdit['input-edit'].value.trim()
+    formEdit.reset()
+    if (todoText.length) {
+        todos.splice(editItemId, 1, {
+            text: todoText,
+            time: getTime(),
+            completed: false,
+        })
+        setTodos()
+        showTodos()
+        close()
+    } else {
+        showMessage('message-edit', 'Please, Enter some todo...')
+    }
+})
+
+// editTodo
+function editTodo(id) {
+    open()
+    editItemId = id
+}
+
+overlay.addEventListener('click', close)
+closeEl.addEventListener('click', close)
+
+document.addEventListener('keydown', (e) => {
+    if (e.which == 27) {
+        close()
+    }
+})
+
+function open() {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+function close() {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
